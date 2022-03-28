d05a585c7fedb0d0eea1fa714970268067871d2d
zmelka <jjdlwodud@gmail.com>
2022-03-28 22:30:47 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ljy/exam1/Exam1Application.java
@@ -0,0 +1,13 @@
+package com.ljy.exam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exam1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljy/exam1/Exam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.exam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
